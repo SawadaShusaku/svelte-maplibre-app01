68b4547d51a8bec7262ed1b35c03e8b3a51fbee1
SawadaShusaku <negamamiura@gmail.com>
2026-04-05 17:53:13 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { 
+    MapLibre, 
+    NavigationControl, 
+    GeolocateControl, 
+    FullScreenControl,
+    Marker,
+  } from 'svelte-maplibre-gl';
+</script>
+
+<MapLibre 
+    class="h-[800px]"
+    style="https://tiles.openfreemap.org/styles/liberty"
+    center={[139.767, 35.681]} 
+    zoom={12}
+    >
+<NavigationControl position="top-right" />
+  <GeolocateControl position="top-right" />
+  <FullScreenControl position="top-right" />
+  <Marker lnglat={[139.69168194810612, 35.68956214184306, ]} />
+</MapLibre>
