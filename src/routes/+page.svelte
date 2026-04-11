ff45ff1165e683c69d0644aa00d3348209c4228a
SawadaShusaku <negamamiura@gmail.com>
2026-04-11 14:39:35 +0900

    revert(src): restore src to first commit snapshot (pre-header state)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,19 +6,16 @@
     FullScreenControl,
     Marker,
   } from 'svelte-maplibre-gl';
-  import 'maplibre-gl/dist/maplibre-gl.css';
 </script>
 
-<div style="height: 800px">
-  <MapLibre 
-      class="h-full w-full"
-      style="https://tiles.openfreemap.org/styles/liberty"
-      center={[139.767, 35.681]} 
-      zoom={12}
-      >
-    <NavigationControl position="top-right" />
-    <GeolocateControl position="top-right" />
-    <FullScreenControl position="top-right" />
-    <Marker lnglat={[139.69168194810612, 35.68956214184306]} />
-  </MapLibre>
-</div>
+<MapLibre 
+    class="h-[800px]"
+    style="https://tiles.openfreemap.org/styles/liberty"
+    center={[139.767, 35.681]} 
+    zoom={12}
+    >
+<NavigationControl position="top-right" />
+  <GeolocateControl position="top-right" />
+  <FullScreenControl position="top-right" />
+  <Marker lnglat={[139.69168194810612, 35.68956214184306, ]} />
+</MapLibre>
